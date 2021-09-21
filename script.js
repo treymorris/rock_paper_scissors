470c7f32e743e4457a13c37fe619557d2832ce6a
Trey Morris <treymorrisit@hotmail.com>
2021-09-21 10:38:46 -0400

    basic layout

--- script.js
+++ script.js
@@ -1,21 +1,51 @@
 //create game that plays against computer
 //create function to select choice for computer
+let computerSelection = Math.floor(Math.random() * 4) + 1;
 
-function computerPlay() {
+if (computerSelection == 1) {
+    computerSelection = "Rock";
+} else if (computerSelection == 2) {
+    computerSelection = "Paper";
+} else  {
+    computerSelection = "Scissors";
+}
     
-    let choice = Math.floor(Math.random() * 4) + 1;
+//collect input from user for their choice
+let playerSelection = prompt("Rock, Paper, or Scissors?");
 
-    if (choice == 1) {
-        console.log("Rock");
-    } else if (choice == 2) {
-        console.log("Paper");
-    } else {
-        console.log("Scissors");
+//create function that declares winner of the round
+function game (computerSelection, playerSelection) {
+    if (computerSelection == playerSelection) {
+        return "It's a tie!";
+    }
+
+    if (playerSelection == "Rock") {
+        if (computerSelection == "Scissors") {
+            return "Computer Wins!";
+        } else {
+            return "Player Wins!";
+        }
+    }
+
+    if (playerSelection == "Paper") {
+        if (computerSelection == "Scissors") {
+            return "Computer Wins!";
+        } else {
+            return "Player Wins!";
+        }
+    }
+
+    if (playerSelection == "Scissors") {
+        if (computerSelection == "Rock") {
+        return "Computer Wins!";
+        } else {
+            return "Player Wins!";
+        }
     }
 }
 
-computerPlay();
+game();
 
-//collect input from user for their choice
-//create function that declares winner of the round
+console.log(game(playerSelection,computerSelection))
+
 //create function that collects win data to keep score and declare winner or loser of five games
